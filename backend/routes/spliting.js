f5a46fcb1cf2be7bb3cd1dd7b5e4bdd9ca547fed
Manu G M <manugmahadeva@gmail.com>
2024-05-06 11:21:31 +0530

    simple changes

--- backend/routes/spliting.js
+++ backend/routes/spliting.js
@@ -2,9 +2,9 @@ const express=require("express");
 const router=express.Router();
 const user_root=require("./user.js")
 const therapist_root=require("./therapist.js")
-const appointment_root=require("./appointment.js")
+
 
 router.use("/user",user_root)
-router.use("/therapist",therapist_root)
-router.use("/appointment",appointment_root);
+router.use("/therapist",therapist_root);
+
 module.exports = router
